10560c88973a565d9a0af106837f26806fe70591
vthglyk <vasgl@intracom-telecom.com>
2018-07-31 10:35:08 +0300

    Added SM integration tests

--- src/test/java/eu/h2020/symbiote/client/l2/SM_IntegrationTests.java
+++ src/test/java/eu/h2020/symbiote/client/l2/SM_IntegrationTests.java
@@ -14,6 +14,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {SymbioteCloudITApplication.class})
 @TestPropertySource(locations = "classpath:application.properties")
@@ -42,6 +50,43 @@ public class SM_IntegrationTests extends ClientFixture {
     public void subscribeSuccess() {
 	    Subscription subscription = new Subscription();
 	    subscription.setPlatformId(platformId);
+        subscription.getResourceType().put("service", false);
+        subscription.getResourceType().put("actuator", false);
         smClient.subscribe(subscription);
+    }
+
+    @Test
+    public void getAllSubscriptions() {
+        Subscription subscription = new Subscription();
+        subscription.setPlatformId(platformId);
+        subscription.getResourceType().put("service", false);
+        subscription.getResourceType().put("actuator", false);
+        smClient.subscribe(subscription);
+	    List<Subscription> result = smClient.getAllSubscriptions().stream()
+                .filter(subscription1 -> subscription1.getPlatformId().equals(platformId))
+                .collect(Collectors.toList());
+
+        assertEquals(platformId, result.get(0).getPlatformId());
+        assertTrue(result.get(0).getResourceType().get("sensor"));
+        assertFalse(result.get(0).getResourceType().get("service"));
+    }
+
+    @Test
+    public void getPlatformSubscriptionsSuccess() {
+        Subscription subscription = new Subscription();
+        subscription.setPlatformId(platformId);
+        subscription.getResourceType().put("service", false);
+        subscription.getResourceType().put("actuator", false);
+        smClient.subscribe(subscription);
+        Subscription result = smClient.getPlatformSubscription(platformId);
+
+        assertEquals(platformId, result.getPlatformId());
+        assertTrue(result.getResourceType().get("sensor"));
+        assertFalse(result.getResourceType().get("service"));
+    }
+
+    @Test(expected = FeignException.class)
+    public void getPlatformSubscriptionsFail() {
+        smClient.getPlatformSubscription("dummy");
     }
 }
